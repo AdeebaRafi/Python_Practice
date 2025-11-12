0ee97a2080ba1b7b03f208e01983dc9c33bdf28b
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2025-11-12 15:21:38 +0500

    commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+def sum(a, b):
+    return(a + b)
+print(sum(3,4))
+
+def sub(c, d):
+    return(c - d)
+print(sub(3,2))
+
+def mul(x,y):
+    return(x * y)
+print(mul(8, 8))
+
+def divide(s,t):
+    return( s/t )
+print(divide(8, 4))
+
