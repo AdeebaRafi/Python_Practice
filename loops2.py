52d7325459281681d06b4bd4eb62565248e9612b
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2025-11-17 23:01:05 +0500

    update

--- loops2.py
+++ loops2.py
@@ -1,21 +1,31 @@
 # Find the sum of numbers from 1 to 50
 # Use a while loop.
 
-total = 50
-number = 1
-while number <= 50:
-    total += number
-    number += 1
-print(f"Sum using while loop: {total}")
-
-count = 0
-for a in range(1,51):
-    count = count + a
-    print(f"Sum using while loop: {count}")
-
-t_sum = sum(range(1,51))
-print(f'use sum method for sum, {t_sum}')
+# total = 50
+# number = 1
+# while number <= 50:
+#     total += number
+#     number += 1
+# print(f"Sum using while loop: {total}")
+#
+# count = 0
+# for a in range(1,51):
+#     count = count + a
+#     print(f"Sum using while loop: {count}")
+#
+# t_sum = sum(range(1,51))
+# print(f'use sum method for sum, {t_sum}')
 
 # Print squares of numbers from 1 to 10
 # (1, 4, 9, 16, …)
+#
+#
+for a in range(1,11):
+    sq = a * a
+    print(sq)
+
+# Count how many odd numbers are in a list
+# Example list
+# [3, 4, 11, 12, 7, 8]
+# Use a for loop and an if.
 
